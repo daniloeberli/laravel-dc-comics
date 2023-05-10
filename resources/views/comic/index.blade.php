1dc1f1c603443c7aa65b7970a69a9eebffefde7a
daniloeberli <danilo.eberli@gmail.com>
2023-05-10 16:57:19 +0200

    feat: full data visualized

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -1,14 +1,44 @@
 @extends('layout.app')
 
 @section('page.title')
-    elenco
+    <div class="container">
+        Comics
+    </div>
 @endsection
 
 @section('page.main')
-<ul>
-    @foreach ($comics as $comic)
-        <li>{{$comic->title}}</li>
+
+<div class="container">
+<table class="table">
+    <thead>
+      <tr>
+
+        <th scope="col">Titolo</th>
+        <th scope="col">Descrizione</th>
+        <th scope="col">Prezzo</th>
+        <th scope="col">Serie</th>
+        <th scope="col">Data</th>
+        <th scope="col">Tipologia</th>
+        {{-- <th scope="col">Artisti</th>
+        <th scope="col">Scrittori</th> --}}
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($comics as $comic)
+        <tr>
+            <td>{{ $comic->title }}</td>
+            <td>{{ $comic->description }} min</td>
+            <td>{{ $comic->price }}</td>
+            <td>{{ $comic->series }}</td>
+            <td>{{ $comic->sale_date }}</td>
+            <td>{{ $comic->type }}</td>
+            {{-- <td>{{ $comic->series }}</td>
+            <td>{{ $comic->series }}</td> --}}
+          </tr>
     @endforeach
-</ul>
+    </tbody>
+  </table>
+</div>
+
     
 @endsection
