c1f23922ab3a3a5526c9ae8453d552612617b7a4
daniloeberli <danilo.eberli@gmail.com>
2023-05-10 15:37:32 +0200

    feat:migration

---
+++ database/migrations/2023_05_10_131446_create_comics_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price',5,2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+            $table->json('artists'); //storing array 
+            $table->json('writers');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
